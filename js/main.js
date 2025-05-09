511409d0a3bed3ee2929313475aefab420a91f1e
ayu-102 <ayuningtiassubartha@gmail.com>
2025-05-09 23:24:21 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+onload = () => {
+    const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+
+        const titles = (' HBD KA FATMA<3 ').split('')
+        const titleElement = document.getElementById('title');
+        let index=0;
+
+        function appendTitle(){
+            if(index < titles.length){
+                titleElement.innerHTML += titles[index];
+                index++;
+                setTimeout(appendTitle,300);
+            }
+        }
+
+        appendTitle();
+        clearTimeout();
+    },1000);
+};
